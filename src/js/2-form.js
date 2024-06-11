27c9d2161d48e2bd644b56896ea1d52e1cc205d3
svitlana_havrylets <svitlana.havrylets@gmail.com>
2024-06-12 00:07:15 +0200

    2-form code - done

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,26 +1,53 @@
 const formElement = document.querySelector('.feedback-form');
-
-// const labels = document.querySelectorAll('.login-form label');
+const inputElement = document.querySelector('input[type="email"]');
+const textareaElement = document.querySelector('textarea[name="message"]');
 
 const formData = {
   email: '',
   message: '',
 };
 
+formElement.addEventListener('input', () => {
+  const email = inputElement.value.trim();
+  const message = textareaElement.value.trim();
+  const formData = { email, message };
+  saveToLS('feedback-form-state', formData);
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  const data = loadFromLS('feedback-form-state');
+
+  inputElement.value = data?.email || '';
+  textareaElement.value = data?.message || '';
+});
+
 formElement.addEventListener('submit', event => {
   event.preventDefault();
-
-  const emailValue = formElement.elements.email.value.trim();
-  const messageValue = formElement.elements.message.value.trim();
-
-  if (emailValue !== '' && messageValue !== '') {
-    const formData = {
-      email: formData.get('email'),
-      message: formData.get('message'),
-    };
+  const email = inputElement.value.trim();
+  const message = textareaElement.value.trim();
+  if (email !== '' && message !== '') {
+    const formData = { email, message };
     console.log(formData);
     formElement.reset();
+    localStorage.removeItem('feedback-form-state');
   } else {
     alert('Fill please all fields');
   }
 });
+
+// ========================================================
+function saveToLS(key, value) {
+  const jsonData = JSON.stringify(value);
+  localStorage.setItem(key, jsonData);
+}
+
+function loadFromLS(key) {
+  const json = localStorage.getItem(key);
+  try {
+    const data = JSON.parse(json);
+    return data;
+  } catch {
+    return json;
+  }
+}
+// ========================================================
